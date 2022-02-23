61341e8c7e975f52f871148628b1700c1b2c2ce3
MINCHAEYEONG <min0703study@gmail.com>
2022-02-23 11:10:42 +0900

    220223 민채영 - 뛰는 액션 왼쪽

--- MUntilWeDie/ImageClass.cpp
+++ MUntilWeDie/ImageClass.cpp
@@ -9,7 +9,7 @@ HRESULT ImageClass::init(void)
 
 	IMAGEMANAGER->addFrameImage(PLAYER_IDLE, "Resources/Images/Player/tex_hero_idle.bmp", PLAYER_X_SIZE * 6, PLAYER_X_SIZE, 6, 1);
 	IMAGEMANAGER->addFrameImage(PLAYER_WALK_R, "Resources/Images/Player/tex_hero_walk_r.bmp", PLAYER_X_SIZE * 8, PLAYER_X_SIZE, 8, 1);
-	IMAGEMANAGER->addFrameImage(PLAYER_WALK_L, "Resources/Images/Player/tex_hero_walk_l.bmp", PLAYER_X_SIZE * 8, PLAYER_X_SIZE, 8, 1);
+	IMAGEMANAGER->addFrameImage(PLAYER_WALK_L, "Resources/Images/Player/tex_hero_run_l.bmp", PLAYER_X_SIZE * 8, PLAYER_X_SIZE, 8, 1);
 
 	return S_OK;
 }
