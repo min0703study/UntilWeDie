b3bd8414af4dc500663318a488db6bcddf1a6255
KimHyeonSeok <lukaleeons@naver.com>
2022-02-23 15:19:10 +0900

    220223 - 김현석 - 이미지 추가 및 빌드매니저 추가

---
+++ MUntilWeDie/BuildManager.cpp
@@ -0,0 +1,21 @@
+#include "Stdafx.h"
+#include "BuildManager.h"
+
+HRESULT BuildManager::init(float x, float y, float width, float height)
+{
+	return S_OK;
+}
+
+void BuildManager::release(void)
+{
+}
+
+void BuildManager::update(void)
+{
+
+}
+
+void BuildManager::render(void)
+{
+
+}
---
+++ MUntilWeDie/BuildManager.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "GameNode.h"
+
+struct BuildRect
+{
+	RECT rc;
+	float x, y;
+	float width;
+	float height;
+};
+
+class BuildManager : public GameNode
+{
+public:
+	HRESULT init(float x, float y, float width, float height);
+	void release(void);
+	void update(void);
+	void render(void);
+
+	BuildManager() {}
+	~BuildManager() {}
+};
+
